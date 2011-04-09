ea26df8b17e7a1d2e48c64d7ec9dbfd84bb9d88b
marianobianchi08 <marianobianchi08@b0bf4047-2539-2db9-b517-8d5f968b1880>
2011-04-09 18:58:29 +0000

    Haciendo el mover

--- codigo/Damas.hs
+++ codigo/Damas.hs
@@ -2,6 +2,12 @@ module Damas where
 
 import Tablero
 
+----------------------- INCLUIDAS POR NOSOTROS---------------------------
+
+import Char
+
+-------------------------------------------------------------------------
+
 data Juego = J Color Tablero
 data Movimiento = M Posicion Direccion
   deriving Show
@@ -29,9 +35,85 @@ arbolDeJugadas j = Nodo ([], j) $ zipWith agmov movs hijos
 ---- Ejercicios ----
 
 -- Ejercicio 3
--- mover :: Movimiento -> Juego -> Maybe Juego
+
 mover :: Movimiento -> Juego -> Maybe Juego
 mover mov jue = Nothing
+{-
+mover :: Movimiento -> Juego -> Maybe Juego
+mover (M pos dir) (J col (T f)) = if cumpleCondiciones then hacerMovimiento else Nothing
+				where
+					cumpleCondiciones = posicionesEnRango && hayFichaEnOrigen && 									    mueveElJugadorCorrespondiente && mueveEnDirCorrecta &&
+							    puedeMover  
+					posicionOrigenEnRango =  enTablero pos
+					posicionDestinoEnRango = if posLlegada == Nothing then False else enTablero posLlegada 
+					noHayFichaEnDestino = 
+					hayFichaEnOrigen = 
+					mueveElJugadorCorrespondiente = 
+					mueveEnDirCorrecta =
+					puedeMover =
+					----
+					hacerMovimiento = if destinoLibre then moverSimple else moverConSalto
+					destinoLibre = ( (f posicionDirecta) == Nothing)
+					moverSimple = Maybe (J (cambiaColor col) tableroDeMueve)
+					moverConSalto = Maybe (J (cambiaColor col) tableroDeSalto)
+					tableroDeMueve = sacar pos (poner posicionDirecta fichaAMover (T f))
+					tableroDeSalto = sacar posDirecta (sacar pos (poner posicionDeSalto fichaAMover (T f)) )
+					posicionDirecta = damePosicion (M pos dir)
+					posicionDeSalto = damePosicion (M posicionDirecta dir)
+					fichaAMover = dameFicha (f pos)
+					----
+						
+-}
+
+-- devuelve la posicion del tablero en donde va a quedar la ficha que se quiere mover
+-- OJO: Si quiere "comer" una ficha propia o se sale de rango, devuelve Nothing
+posLlegada :: Movimiento -> Juego -> Maybe Posicion
+posLlegada (M pos dir) (J col (T f)) = if posDirectaLibre then Just posDirecta else posNoDirecta 
+		where 
+			posDirecta = posDeMoverDirecto (M pos dir)
+			posNoDirecta = if posNoDirectaLibre && comidaDisponible then Just posDeMoverDirecto (M posicionDeMovimientoDirecto dir) else Nothing
+
+
+enTablero :: Posicion -> Bool
+enTablero p = elem p posicionesValidas
+{-}if (saleDeRango posicionFinal) then Nothing else posicionFinal
+						where 
+							saleDeRango = \mp -> elem (damePosicion mp) posicionesValidas
+							posicionFinal = if posDirectaLibre then posDirecta else posDeComidaSiCome 
+							posDirectaLibre = f (posicionDeMovimientoDirecto) == Nothing
+							posDirecta = (Just posicionDeMovimientoDirecto )
+							posicionDeMovimientoDirecto = posDeMoverDirecto (M pos dir)
+							posicionDeComidaSiCome = posDeMoverDirecto (M posicionDeMovimientoDirecto dir)
+							posDeComidaSiCome = if hayFichaOponente then (Just posicionDeComidaSiCome) else Nothing
+							hayFichaOponente = col /= colorDeFicha (dameFicha (f posicionDeMovimientoDirecto))
+-}							
+						
+--dado un movimiento, devuelve la posicion en donde "desembocaria" ese movimiento (si hay ficha)
+posDeMoverDirecto :: Movimiento -> Posicion
+posDeMoverDirecto (M pos TL) = ( chr (ord (fst pos) - 1), (snd pos) + 1 )
+posDeMoverDirecto (M pos TR) = ( chr (ord (fst pos) + 1), (snd pos) + 1 )
+posDeMoverDirecto (M pos BL) = ( chr (ord (fst pos) - 1), (snd pos) - 1 )
+posDeMoverDirecto (M pos BR) = ( chr (ord (fst pos) + 1), (snd pos) - 1 )
+
+--cambia el color
+cambiaColor :: Color -> Color
+cambiaColor Blanca = Negra
+cambiaColor Negra = Blanca
+
+--devuelve la ficha de una Maybe Ficha (ES PRECONDICION QUE SEA JUST)
+dameFicha :: Maybe Ficha -> Ficha
+dameFicha (Just f) = f
+
+--devuelve la posicion de una Maybe Posicion (ES PRECONDICION QUE SEA JUST)
+damePosicion :: Maybe Posicion -> Posicion
+damePosicion (Just p) = p
+
+--devuelve el color de una ficha
+colorDeFicha :: Ficha -> Color
+colorDeFicha (Simple c) = c
+colorDeFicha (Reina c) = c
+
+
 
 -- Ejercicio 4
 movimientosPosibles :: Juego -> [(Movimiento, Juego)]
